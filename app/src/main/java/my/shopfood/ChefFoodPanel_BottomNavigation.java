67537daefd889fd085f6a668ea82f63c19b7d909
samikshyakandel <samikshyakandelbca076@indrenicollege.edu.np>
2023-07-13 21:55:13 +0545

    project clz

--- app/src/main/java/my/shopfood/ChefFoodPanel_BottomNavigation.java
+++ app/src/main/java/my/shopfood/ChefFoodPanel_BottomNavigation.java
@@ -1,14 +1,56 @@
 package my.shopfood;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class ChefFoodPanel_BottomNavigation extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener;
+
+import my.shopfood.chefFoodPanel.ChefHomeFragment;
+import my.shopfood.chefFoodPanel.ChefOrderFragment;
+import my.shopfood.chefFoodPanel.ChefPendingOrderFragment;
+import my.shopfood.chefFoodPanel.ChefProfileFragment;
+
+public class ChefFoodPanel_BottomNavigation extends AppCompatActivity  implements OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chef_food_panel_bottom_navigation);
+    BottomNavigationView navigationView=findViewById(R.id.chef_bottom_navigation);
+    navigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment=null;
+        switch (item.getItemId()){
+            case R.id.chefHome:
+                 fragment = new ChefHomeFragment();
+                break;
+            case R.id.pendingOrders:
+                fragment=new ChefPendingOrderFragment();
+                break;
+            case R.id.Orders:
+                fragment=new ChefOrderFragment();
+                break;
+            case R.id.chefProfile:
+                fragment=new ChefProfileFragment();
+                break;
+
+        }
+        return loadcheffragment(fragment);
+    }
+
+    private boolean loadcheffragment(Fragment fragment) {
+        if (fragment != null){
+         getSupportFragmentManager().beginTransaction().replace(R.id.fragement_container,fragment).commit();
+         return true;
+        }
+        return false;
     }
 }
