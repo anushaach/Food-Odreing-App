020622912cc4d5d2ceb7074373c6654a0b01c35c
samikshyakandel <samikshyakandelbca076@indrenicollege.edu.np>
2023-07-19 01:02:15 +0545

    project clz

--- app/src/main/java/my/shopfood/chefFoodPanel/chef_postDish.java
+++ app/src/main/java/my/shopfood/chefFoodPanel/chef_postDish.java
@@ -1,14 +1,101 @@
 package my.shopfood.chefFoodPanel;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import my.shopfood.R;
 
 public class chef_postDish extends AppCompatActivity {
+
+    ImageButton imageButton;
+    Button post_dish;
+    Spinner Dishes;
+    TextInputLayout desc, qty,pri;
+    String description,quantity,price,dishes;
+    Uri imageuri;
+    private Uri mcropimageuri;
+    FirebaseStorage storage;
+    StorageReference storageReference;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference, dataa;
+    FirebaseAuth Fauth;
+    StorageReference ref;
+    String ChefId, RandomUID, State, City, Area;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chef_post_dish);
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+        Dishes = (Spinner) findViewById(R.id.dishes);
+        desc = (TextInputLayout) findViewById(R.id.description);
+        qty = (TextInputLayout) findViewById(R.id.Quantity);
+        pri = (TextInputLayout) findViewById(R.id.price);
+        post_dish = (Button) findViewById(R.id.post_dish);
+        Fauth = FirebaseAuth.getInstance();
+        databaseReference = firebaseDatabase.getInstance().getReference("FoodDetails");
+
+        try{
+            String userid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+            dataa = firebaseDatabase.getInstance().getReference("Chef");
+            dataa.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                  Chef cheff = snapshot.getValue(Chef.class);
+                  State= cheff.getState();
+                  City = cheff.getCity();
+                  Area = cheff.getArea();
+                  imageButton = (ImageButton) findViewById(R.id.imageupload);
+                  imageButton.setOnClickListener(new View.OnClickListener() {
+                      @Override
+                      public void onClick(View view) {
+
+                       dishes = Dishes.getSelectedItem().toString().trim();
+                       description = desc.getEditText().getText().toString().trim();
+                       quantity = qty.getEditText().getText().toString().trim();
+                       price = pri.getEditText().getText().toString().trim();
+
+                       if(isValid()){
+                           uploadaimage();
+                       }
+                      }
+                  });
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }catch (Exception e){
+            Log.e("Error:", getMessage());
+        }
+    }
+
+    private boolean isValid() {
     }
 }
