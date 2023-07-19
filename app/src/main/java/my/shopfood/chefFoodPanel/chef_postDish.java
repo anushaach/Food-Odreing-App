19c5d9a737b074e34dc9be7e89e683579f290942
samikshyakandel <samikshyakandelbca076@indrenicollege.edu.np>
2023-07-20 01:09:34 +0545

    project clz

--- app/src/main/java/my/shopfood/chefFoodPanel/chef_postDish.java
+++ app/src/main/java/my/shopfood/chefFoodPanel/chef_postDish.java
@@ -3,15 +3,22 @@ package my.shopfood.chefFoodPanel;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -19,8 +26,13 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.UUID;
 
 import my.shopfood.R;
 
@@ -29,8 +41,8 @@ public class chef_postDish extends AppCompatActivity {
     ImageButton imageButton;
     Button post_dish;
     Spinner Dishes;
-    TextInputLayout desc, qty,pri;
-    String description,quantity,price,dishes;
+    TextInputLayout desc, qty, pri;
+    String description, quantity, price, dishes;
     Uri imageuri;
     private Uri mcropimageuri;
     FirebaseStorage storage;
@@ -57,32 +69,32 @@ public class chef_postDish extends AppCompatActivity {
         Fauth = FirebaseAuth.getInstance();
         databaseReference = firebaseDatabase.getInstance().getReference("FoodDetails");
 
-        try{
+        try {
             String userid = FirebaseAuth.getInstance().getCurrentUser().getUid();
             dataa = firebaseDatabase.getInstance().getReference("Chef");
             dataa.addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
 
-                  Chef cheff = snapshot.getValue(Chef.class);
-                  State= cheff.getState();
-                  City = cheff.getCity();
-                  Area = cheff.getArea();
-                  imageButton = (ImageButton) findViewById(R.id.imageupload);
-                  imageButton.setOnClickListener(new View.OnClickListener() {
-                      @Override
-                      public void onClick(View view) {
+                    Chef cheff = snapshot.getValue(Chef.class);
+                    State = cheff.getState();
+                    City = cheff.getCity();
+                    Area = cheff.getArea();
+                    imageButton = (ImageButton) findViewById(R.id.imageupload);
+                    imageButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
 
-                       dishes = Dishes.getSelectedItem().toString().trim();
-                       description = desc.getEditText().getText().toString().trim();
-                       quantity = qty.getEditText().getText().toString().trim();
-                       price = pri.getEditText().getText().toString().trim();
+                            dishes = Dishes.getSelectedItem().toString().trim();
+                            description = desc.getEditText().getText().toString().trim();
+                            quantity = qty.getEditText().getText().toString().trim();
+                            price = pri.getEditText().getText().toString().trim();
 
-                       if(isValid()){
-                           uploadaimage();
-                       }
-                      }
-                  });
+                            if (isValid()) {
+                                uploadimage();
+                            }
+                        }
+                    });
 
                 }
 
@@ -91,11 +103,95 @@ public class chef_postDish extends AppCompatActivity {
 
                 }
             });
-        }catch (Exception e){
-            Log.e("Error:", getMessage());
+        } catch (Exception e) {
+            Log.e("Error:", e.getMessage());
         }
     }
 
-    private boolean isValid() {
+    private void uploadimage() {
+
+        if (imageuri != null) {
+            final ProgressDialog progressDialog = new ProgressDialog(chef_postDish.this);
+            progressDialog.setTitle("Uploading.........");
+            progressDialog.show();
+            RandomUID = UUID.randomUUID().toString();
+            ref = storageReference.child(RandomUID);
+            ChefId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+            ref.putFile(imageuri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            FoodDetails info = new FoodDetails(dishes, quantity, price, description, String.valueOf(uri), RandomUID, ChefId);
+                            Task<Void> foodDetails = firebaseDatabase.getInstance().getReference("FoodDetails").child(State).child(City).child(Area).child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child(RandomUID).setValue(info).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+
+                                    progressDialog.dismiss();
+                                    Toast.makeText(chef_postDish.this, "Dish Posted Successfully!", Toast.LENGTH_SHORT).show();
+
+
+                                }
+                            });
+
+
+                        }
+                    });
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    progressDialog.dismiss();
+                    Toast.makeText(chef_postDish.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                    FileDownloadTask.TaskSnapshot taskSnapshot = null;
+                    double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                    progressDialog.setCanceledOnTouchOutside(false);
+
+
+                }
+            });
+
+        }
     }
+
+
+    private boolean isValid() {
+
+        desc.setErrorEnabled(false);
+        desc.setError("");
+        qty.setErrorEnabled(false);
+        qty.setError("");
+        pri.setErrorEnabled(false);
+        pri.setError("");
+
+        boolean isvalidDescription = false, isValidPrice = false, isValidQuality = false, isValid = false;
+        if (TextUtils.isEmpty(description)) {
+            desc.setErrorEnabled(true);
+            desc.setError("Description is Required");
+        } else {
+            desc.setError(null);
+            isvalidDescription = true;
+        }
+        boolean isValidQuantity = false;
+        if (TextUtils.isEmpty(quantity)) {
+            qty.setErrorEnabled(true);
+            qty.setError("Enter number of Plates or Items");
+        } else {
+            isValidQuantity = true;
+        }
+        if (TextUtils.isEmpty(price)) {
+            pri.setErrorEnabled(true);
+            pri.setError("Please Mention Price");
+        } else {
+            isValidPrice = true;
+        }
+        isValid = (isvalidDescription && isValidQuantity && isValidPrice) ? true : false;
+        return isValid;
+    }
+
 }
