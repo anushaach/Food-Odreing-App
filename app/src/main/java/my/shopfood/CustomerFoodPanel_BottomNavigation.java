eead9e3f73abe0846af84a4c6a1cdfdb1a3184da
Anusha Acharya <104045774+anushaach@users.noreply.github.com>
2023-07-28 23:56:49 +0545

    clz project

--- app/src/main/java/my/shopfood/CustomerFoodPanel_BottomNavigation.java
+++ app/src/main/java/my/shopfood/CustomerFoodPanel_BottomNavigation.java
@@ -1,14 +1,23 @@
 package my.shopfood;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class CustomerFoodPanel_BottomNavigation extends AppCompatActivity {
+public class CustomerFoodPanel_BottomNavigation extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected  void onCreate(Bundle savedInstanceState){
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_food_panel_bottom_navigation);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
