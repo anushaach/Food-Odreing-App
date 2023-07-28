8e68dde39068312b9a3e4ed7ee8414cc1618016c
Anusha Acharya <104045774+anushaach@users.noreply.github.com>
2023-07-28 23:41:47 +0545

    clz project

--- app/src/main/java/my/shopfood/customerFoodPanel/CustomerCartFragment.java
+++ app/src/main/java/my/shopfood/customerFoodPanel/CustomerCartFragment.java
@@ -1,4 +1,20 @@
 package my.shopfood.customerFoodPanel;
 
-public class CustomerCartFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import my.shopfood.R;
+
+public class CustomerCartFragment extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_customercart, null);
+        getActivity().setTitle("Cart");
+        return v;
+    }
 }
--- app/src/main/java/my/shopfood/customerFoodPanel/CustomerOdersFragment.java
+++ app/src/main/java/my/shopfood/customerFoodPanel/CustomerOdersFragment.java
@@ -1,7 +1,24 @@
 package my.shopfood.customerFoodPanel;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import my.shopfood.R;
 
 public class CustomerOdersFragment extends Fragment {
 
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v= inflater.inflate(R.layout.fragment_customeroders,null);
+        getActivity().setTitle("Oders");
+        return v;
+    }
 }
