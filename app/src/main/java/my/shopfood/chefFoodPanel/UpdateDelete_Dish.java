c14fc6711367e6c88e0718322169f4c13c41f3f5
samikshyakandel <samikshyakandelbca076@indrenicollege.edu.np>
2023-08-03 09:06:38 +0545

    project clz

---
+++ app/src/main/java/my/shopfood/chefFoodPanel/UpdateDelete_Dish.java
@@ -0,0 +1,278 @@
+package my.shopfood.chefFoodPanel;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.UUID;
+
+import my.shopfood.ChefFoodPanel_BottomNavigation;
+import my.shopfood.R;
+import my.shopfood.UpdateDishModel;
+
+public class UpdateDelete_Dish extends AppCompatActivity {
+    TextInputLayout desc, qty, pri;
+    TextView Dishname;
+    ImageButton imageButton;
+    Uri imageuri;
+    String dburi;
+    private Uri mCropimageuri;
+    Button Update_dish, Delete_dish;
+    String description, quantity, price, dishes, chefId;
+    String RandomUID;
+    StorageReference storageReference;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    FirebaseAuth FAuth;
+    String ID;
+    private ProgressDialog progressDialog;
+    DatabaseReference dataa;
+    String State, City, Area;
+    private StorageReference ref;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_delete_dish);
+
+        desc = (TextInputLayout) findViewById(R.id.description);
+        qty = (TextInputLayout) findViewById(R.id.quantity);
+        pri = (TextInputLayout) findViewById(R.id.price);
+        Dishname = (TextView) findViewById(R.id.dish_name);
+        imageButton = (ImageButton) findViewById(R.id.imageupload);
+        Update_dish = (Button) findViewById(R.id.Updatedish);
+        Delete_dish = (Button) findViewById(R.id.Deletedish);
+        ID = getIntent().getStringExtra("updatedeletedish");
+        String userid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        dataa = firebaseDatabase.getInstance().getReference("chef").child(userid);
+        dataa.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Chef cheff = snapshot.getValue(Chef.class);
+                State = cheff.getState();
+                City = cheff.getCity();
+                Area = cheff.getArea();
+
+                Update_dish.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        description = desc.getEditText().getText().toString().trim();
+                        quantity = qty.getEditText().getText().toString().trim();
+                        price = pri.getEditText().getText().toString().trim();
+
+                        if (isValid()) {
+                            if (imageuri != null) {
+                                UploadImage();
+                            } else {
+                                updatedesc(dburi);
+                            }
+                        }
+                    }
+                });
+                Delete_dish.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        AlertDialog.Builder builder=new AlertDialog.Builder(UpdateDelete_Dish.this);
+                        builder.setMessage("Are you sure you want to Delete Dish");
+                        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                firebaseDatabase.getInstance().getReference("FoodDetails").child(State).child(City).child(Area)
+                                        .child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child(ID).removeValue();
+                                AlertDialog.Builder food=new AlertDialog.Builder(UpdateDelete_Dish.this);
+                                food.setMessage("Your Dish has been Deleted!");
+                                food.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        startActivity(new Intent(UpdateDelete_Dish.this, ChefFoodPanel_BottomNavigation.class));
+                                    }
+                                });
+                                AlertDialog alert= food.create();
+                                alert.show();;
+                            }
+                        });
+                        builder.setNegativeButton("HD", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        });
+                        AlertDialog alert=builder.create();
+                        alert.show();
+
+                    }
+                });
+                String useridd = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                progressDialog= new ProgressDialog(UpdateDelete_Dish.this);
+                databaseReference=FirebaseDatabase.getInstance().getReference("FoodDetails").child(State).child(City).child(Area)
+                        .child(useridd).child(ID);
+                databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        UpdateDishModel updateDishModel=snapshot.getValue(UpdateDishModel.class);
+                        desc.getEditText().setText(updateDishModel.getDescription());
+                        qty.getEditText().setText(updateDishModel.getQuantity());
+                        Dishname.setText("Dish name: "+updateDishModel.getDishes());
+                        dishes=updateDishModel.getDishes();
+                        pri.getEditText().setText(updateDishModel.getPrice());
+                        dburi=updateDishModel.getImageURL();
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+                FAuth=FirebaseAuth.getInstance();
+                databaseReference=firebaseDatabase.getInstance().getReference("FoodDetails");
+                FirebaseStorage storage = FirebaseStorage.getInstance();
+                storageReference=storage.getReference();
+                imageButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        onSelectClick(view);
+                    }
+                });
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void onSelectClick(View view) {
+    }
+
+    private void updatedesc(String dburi) {
+        chefId= FirebaseAuth.getInstance().getCurrentUser().getUid();
+        FoodDetails info= new FoodDetails(dishes,quantity,price,description,dburi,ID,chefId);
+        firebaseDatabase.getInstance().getReference("FoodDetails").child(State).child(City).child(Area)
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child(ID)
+                .setValue(info).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        progressDialog.dismiss();
+                        Toast.makeText(UpdateDelete_Dish.this, "Dish Upadtes Sucessfully", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+    }
+
+    private void UploadImage() {
+        if(imageuri !=null){
+            progressDialog.setTitle("Uploading....");
+            progressDialog.show();
+            RandomUID= UUID.randomUUID().toString();
+            storageReference=storageReference.child(RandomUID);
+            ref.putFile(imageuri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>(){
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>(){
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            updatedesc(String.valueOf(uri));
+                        }
+
+
+
+
+
+
+                    });
+
+                }
+            }).addOnFailureListener(new OnFailureListener(){
+                @Override
+                public void onFailure(@NonNull Exception e) {
+
+                }
+
+                public  void OnFaliure(@NonNull Exception e){
+                    progressDialog.dismiss();
+                    Toast.makeText(UpdateDelete_Dish.this,"Failed:" +e.getMessage(),Toast.LENGTH_LONG).show();
+
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>(){
+                @Override
+                public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot){
+                    double progress=(100.0 * taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
+                    progressDialog.setMessage("Upload" + (int) progress+ "%");
+                    progressDialog.setCanceledOnTouchOutside(false);
+
+
+                }
+            });
+        }
+    }
+
+    private boolean isValid() {
+
+        desc.setErrorEnabled(false);
+        desc.setError("");
+        qty.setErrorEnabled(false);
+        qty.setError("");
+        pri.setErrorEnabled(false);
+        pri.setError("");
+
+        boolean isvalidDescription = false, isValidPrice = false, isValidQuality = false, isValid = false, isValidQuantity = false;;
+        if (TextUtils.isEmpty(description)) {
+            desc.setErrorEnabled(true);
+            desc.setError("Description is Required");
+        } else {
+            desc.setError(null);
+            isvalidDescription = true;
+        }
+
+        if (TextUtils.isEmpty(quantity)) {
+            qty.setErrorEnabled(true);
+            qty.setError("Enter number of Plates or Items");
+        } else {
+            isValidQuantity = true;
+        }
+        if (TextUtils.isEmpty(price)) {
+            pri.setErrorEnabled(true);
+            pri.setError("Please Mention Price");
+        } else {
+            isValidPrice = true;
+        }
+        isValid = (isvalidDescription && isValidQuantity && isValidPrice) ? true : false;
+        return isValid;
+    }
+
+
+}
